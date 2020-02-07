d2c587ee78fb17aba3a6fc206197dcb98dd4e10d
shrayanbajra <shrayanbajracharya@gmail.com>
2020-02-07 19:16:14 +0545

    NavController: Defined item click actions on buttons of MainFragment

--- app/src/main/java/com/example/android/jetpacknav/MainFragment.kt
+++ app/src/main/java/com/example/android/jetpacknav/MainFragment.kt
@@ -5,12 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var navController: NavController
+
+    private lateinit var btnViewTransaction: Button
+    private lateinit var btnSendMoney: Button
+    private lateinit var btnViewBalance: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +29,34 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        navController = Navigation.findNavController(view)
+
+        btnViewTransaction = view.findViewById(R.id.btn_view_transaction)
+        btnSendMoney = view.findViewById(R.id.btn_send_money)
+        btnViewBalance = view.findViewById(R.id.btn_view_balance)
+
+        btnViewTransaction.setOnClickListener(this)
+        btnSendMoney.setOnClickListener(this)
+        btnViewBalance.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+
+            R.id.btn_view_transaction -> navController.navigate(
+                R.id.action_mainFragment_to_viewTransactionFragment
+            )
+
+            R.id.btn_send_money -> navController.navigate(
+                R.id.action_mainFragment_to_chooseRecipientFragment
+            )
+
+            R.id.btn_view_balance -> navController.navigate(
+                R.id.action_mainFragment_to_viewBalanceFragment
+            )
+        }
+    }
 }

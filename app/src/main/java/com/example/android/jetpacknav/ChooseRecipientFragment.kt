c6b44ee60e61e8f0b8050cbae20ecd5fe26c1113
shrayanbajra <shrayanbajracharya@gmail.com>
2020-02-07 19:25:34 +0545

    NavController: Defined item click actions on buttons of ChooseRecipient Fragment

--- app/src/main/java/com/example/android/jetpacknav/ChooseRecipientFragment.kt
+++ app/src/main/java/com/example/android/jetpacknav/ChooseRecipientFragment.kt
@@ -5,12 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class ChooseRecipientFragment : Fragment() {
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var btnNext: Button
+    private lateinit var btnCancelTransaction: Button
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +28,25 @@ class ChooseRecipientFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        btnNext = view.findViewById(R.id.btn_next)
+        btnCancelTransaction = view.findViewById(R.id.btn_cancel_transaction)
+
+        navController = Navigation.findNavController(view)
+
+        btnNext.setOnClickListener(this)
+        btnCancelTransaction.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+
+            R.id.btn_next -> navController.navigate(
+                R.id.action_chooseRecipientFragment_to_specifyAmountFragment
+            )
+            R.id.btn_cancel_transaction -> activity?.onBackPressed()
+        }
+    }
 }

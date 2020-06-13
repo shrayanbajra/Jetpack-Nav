a5017e2093ba8f1f7776db6a350297f8ea2485a3
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-06-13 17:06:47 +0545

    Simplified code

--- app/src/main/java/com/example/android/jetpacknav/MainFragment.kt
+++ app/src/main/java/com/example/android/jetpacknav/MainFragment.kt
@@ -1,16 +1,17 @@
 package com.example.android.jetpacknav
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass.
- */
 class MainFragment : Fragment() {
+
+    private lateinit var btnNavigateToViewBalance: Button
+    private lateinit var btnNavigateToViewTransactions: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +19,29 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnNavigateToViewBalance = view.findViewById(R.id.btn_navigate_to_view_balance)
+        btnNavigateToViewTransactions = view.findViewById(R.id.btn_navigate_to_view_transactions)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnNavigateToViewBalance.setOnClickListener {
+            // Navigate to ViewBalanceFragment
+            findNavController().navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
+
+
+        btnNavigateToViewTransactions.setOnClickListener {
+            // Navigate to ViewTransactionsFragment
+            findNavController().navigate(R.id.action_mainFragment_to_viewTransactionsFragment)
+        }
     }
 
 
--- app/src/main/java/com/example/android/jetpacknav/ViewTransactionsFragment.kt
+++ app/src/main/java/com/example/android/jetpacknav/ViewTransactionsFragment.kt
@@ -10,14 +10,14 @@ import androidx.fragment.app.Fragment
 /**
  * A simple [Fragment] subclass.
  */
-class SpecifyAmountFragment : Fragment() {
+class ViewTransactionsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+        return inflater.inflate(R.layout.fragment_view_transactions, container, false)
     }
 
 
